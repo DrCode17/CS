22aae64743baac2685b9351b785917e4fcb66041
Peccat <80050651+Peccat@users.noreply.github.com>
2021-12-16 14:57:39 +0100

    hazi file-ok(üresek)

---
+++ 13. hazi.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program{
+    public static void Main(){
+        /*
+            A feladatokhoz, ha kell hozhatsz létre segéd változókat.
+            Válaszokat szöveges formában írasd ki a console-ra és elé a feladatot, hogy mi volt a feladat.
+            Szorgalmi feladatban lehet olyan, amit még nem vettünk, így az egy kis keresgéléssel járhat!
+            Jó gyakorlást!
+
+            1. feladat: 
+            2. feladat: 
+            3. feladat: 
+            4. feladat: 
+            5. feladat: 
+
+            -----------------Szorgalmi----------------------
+            1. feladat: 
+            2. feladat: 
+            ------------------------------------------------ 
+        */
+
+        
+
+    }
+}
