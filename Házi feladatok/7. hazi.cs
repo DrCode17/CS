7174384ab59f8a2c2d08a4783a0dcccf03596cb9
Peccat <80050651+Peccat@users.noreply.github.com>
2022-01-31 15:20:46 +0100

    01.31. 1520

--- Házi feladatok/7. hazi.cs
+++ Házi feladatok/7. hazi.cs
@@ -8,15 +8,17 @@ class Program{
             Szorgalmi feladatban lehet olyan, amit még nem vettünk, így az egy kis keresgéléssel járhat!
             Jó gyakorlást!
 
-            1. feladat: 
-            2. feladat: 
-            3. feladat: 
-            4. feladat: 
-            5. feladat: 
+            1. feladat: Olvassa be a 7.1. hazi.txt állományt, amelyekben számok találhatóak. Adja össze az öszes számot, számolja ki az átlagukat
+                        a számoknak, felváltva adja hozzá, majd vonja ki a következőt.
+
+            2. feladat: Olvassa be és tárolja el az adatokat a 7.2. hazi.txt-ből, amjd írja ki hány sort tartalmaz.
 
             -----------------Szorgalmi----------------------
-            1. feladat: 
-            2. feladat: 
+            1. feladat: Olvassa be és tárolja el a 7.3. hazi.txt állományt. Írja ki hány sort tartalmaz, majd kérjen be egy számot és írja ki
+                        melyik betű áll azon a helyen. A betűk értékét adja össze (A = 1), majd írja ki az értéküket.
+                        
+            2. feladat: Ha az első kész, akkor mindegyik értékét írd ki: A=1; B=3 (A+B = 1+2); C=6(A+B+C = 1+2+3)... Majd add őssze őket és írd ki az
+                        eredményt. 
             ------------------------------------------------ 
         */
 
--- Házi feladatok/8. hazi.cs
+++ Házi feladatok/8. hazi.cs
@@ -8,15 +8,18 @@ class Program{
             Szorgalmi feladatban lehet olyan, amit még nem vettünk, így az egy kis keresgéléssel járhat!
             Jó gyakorlást!
 
-            1. feladat: 
-            2. feladat: 
-            3. feladat: 
-            4. feladat: 
-            5. feladat: 
+            1. feladat: Olvassa be és tárolja el az adatokat. Az adatok 3-as tagolásban annak (1. házszám, 2. szélesség, 3. hosszúság), 
+                        viszont külön sorokban. 700 négyzetméter vagy az alatt 51 Fabatka négyzetméterenként, felette 45 Fabatka 1000
+                        négyzetméterig és 1000 négyzetméter felett 200 Fabatka az alap díj és mellette 39 Fabatka négyzetméterenként.
+                        Melyik házszámhoz mekkora adó tartozik? Az adó kiszámolásához írjon egy függvényt!
+            2. feladat: Olassa be és tárolja el az adatokat(8.2. hazi.txt), majd írja ki hány soros az adatforrás. Számolja meg melyik szó hány betűből áll és
+                        Adja össze őket. Majd számolja ki az átlagukat és írja ki átlagban milyen hosszú egy szó (3,6 hosszú nincs, így a 
+                        képernyőn 3-4 jelenjen meg)
 
             -----------------Szorgalmi----------------------
-            1. feladat: 
-            2. feladat: 
+            1. feladat: A 8.2. hazi.txt szavaiból írja ki az első és az utolső betűket soronként, majd a köztük levő távolságot.
+                        a-a = 0; a-b = 1; a-c = 2; a-d = 3...
+                        Végén adaj össze az összes távolságot és írja ki a képernyőre az összegüket.  
             ------------------------------------------------ 
         */
 
